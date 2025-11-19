b051d81698d71a2d1b8269ce49b4d3b06dc96b8b
camilo ruiz <camiloruizvidal@gmail.com>
2025-11-19 11:14:22 -0500

    Genenando cuentas de cobros

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './infrastructure/persistence/database/database.module';
@@ -6,10 +7,19 @@ import { KafkaModule } from './infrastructure/messaging/kafka/kafka.module';
 import { CuentasCobroService } from './application/services/cuentas-cobro.service';
 import { CuentasCobroController } from './presentation/controllers/cuentas-cobro.controller';
 import { ManejadorError } from './utils/manejador-error/manejador-error';
+import { JwtTenantGuard } from './presentation/guards/jwt-tenant.guard';
+import { Config } from './infrastructure/config/config';
 
 @Module({
-  imports: [DatabaseModule, KafkaModule],
+  imports: [
+    DatabaseModule,
+    KafkaModule,
+    JwtModule.register({
+      secret: Config.jwtKey,
+      signOptions: { expiresIn: '12h' },
+    }),
+  ],
   controllers: [AppController, CuentasCobroController],
-  providers: [AppService, CuentasCobroService, ManejadorError],
+  providers: [AppService, CuentasCobroService, ManejadorError, JwtTenantGuard],
 })
 export class AppModule {}
